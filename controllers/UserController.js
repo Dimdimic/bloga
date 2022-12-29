e70e7a9484f90745fd159815afc6944c308c10ef
Dmitry <dmitrvad9@gmail.com>
2022-12-30 00:32:39 +0300

    createalid

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,16 +1,11 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import {validationResult} from "express-validator";
+
 import UserModel from "../models/User.js";
 
 export const register = async (req, res) => {
-    try {
-     const errors = validationResult(req);
-     if (!errors.isEmpty()) { //ошибки есть
-         return res.status(400).json(errors.array());
-     }
- 
-     const password = req.body.password; //открытый пароль
+    try {  
+      const password = req.body.password; //открытый пароль
      const salt = await bcrypt.genSalt(10); //метод шифрования
      const hash = await bcrypt.hash(password, salt); //зашифрованный пароль
  
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import checkAuth from "./utils/checkAuth.js";
 
 import {register, login, getMe} from "./controllers/UserController.js";
 import * as PostController from "./controllers/PostController.js";
+import handleValidationErrors from "./utils/handleValidationErrors.js";
 
 
 
@@ -23,25 +24,37 @@ const app = express();
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
         cb(null, 'uploads');
-    }
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
 });
 
+const upload = multer({ storage });
+
 app.use(express.json()); 
+app.use('/uploads', express.static('uploads'));
 
 //Авторизация
-app.post('/auth/login', loginValidation, login);
+app.post('/auth/login', loginValidation, handleValidationErrors, login);
 
 //Регистрация
-app.post('/auth/register', registerValidation, register);
+app.post('/auth/register',  registerValidation, handleValidationErrors, register);
 
 //Информация о пользователе
 app.get('/auth/me', checkAuth, getMe);
 
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
+
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
-app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, PostController.update);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
 app.listen(4444, (err) => {
     if (err) {
